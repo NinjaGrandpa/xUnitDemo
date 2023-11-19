a6a56d0777777854c3d8a6df0250a0b2e0428bb2
NinjaGrandpa <34811547+NinjaGrandpa@users.noreply.github.com>
2023-11-19 13:17:11 +0100

    Fix

--- xUnitDemo/Calculator.cs
+++ xUnitDemo/Calculator.cs
@@ -4,7 +4,7 @@ public class Calculator : ICalculator
 {
     public decimal Add(decimal a, decimal b)
     {
-        return 3;
+        return a + b;
     }
 
     public decimal Subtract(decimal a, decimal b)
@@ -14,7 +14,7 @@ public class Calculator : ICalculator
 
     public decimal Multiply(decimal a, decimal b)
     {
-        throw new NotImplementedException();
+        return a * b;
     }
 
     public decimal Divide(decimal a, decimal b)
